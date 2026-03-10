c19216f6528d120165ee285eb33ee247aa29e350
Colm Nolan <182516888+ColmN-Dev@users.noreply.github.com>
2026-03-10 17:58:55 +0000

    Update site URL in astro.config.mjs

--- Astro-Static-Project/astro.config.mjs
+++ Astro-Static-Project/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-    site: 'https://Silv3rP.github.io',
+    site: 'https://ColmN-Dev.github.io',
     base: '/Astro-Static-Project/',
 });
